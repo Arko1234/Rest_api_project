2844a8f4cc0f0b6866f32f5971b160ed04a0ff91
DESKTOP-0P0FO3O\LENOVO <arko.sxc.96@gmail.com>
2021-08-03 03:23:15 +0530

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const UserModel = require('../model/UserModel')
+const userModel = require('../model/UserModel')
+const router = express.Router()
+
+//Get all the users
+router.get('/', async (req, res) =>{
+    try{
+       const users = await UserModel.find()
+       res.json(users)
+
+    }catch(err){
+         console.log({message:err})
+    }
+})
+
+//Created the users
+
+router.post('/', (req, res) =>{
+   const user = new UserModel({
+       title:req.body.title,
+       description:req.body.description
+   })
+   user.save().then(data=>res.send(data)).catch(err => console.error(err, 'failed to save to the database'))
+})
+
+//Get individual users
+router.get('/:userId', async (req,res) => {
+    try{
+         const user = await UserModel.findById(req.params.userId)
+         res.json(user)
+    }catch(err){
+         res.json({message:err})
+    }
+})
+
+//Update the user
+router.patch('/:userId', async (req, res) => {
+    try{
+         const updateUser = await UserModel.updateOne({_id:req.params.userId}, {$set:{title:req.body.title, description:req.body.description}})
+         res.json(updateUser)
+    }catch(err){
+         res.json({message:err})
+    }
+})
+
+//Delete the user
+router.delete('/:userId', async (req, res) =>{
+    try{
+        const deleteUser = await UserModel.remove({_id:req.params.userId})
+        res.json(deleteUser)
+    }catch(err){
+        res.json({message:err})
+    }
+} )
+
+module.exports = router
+
